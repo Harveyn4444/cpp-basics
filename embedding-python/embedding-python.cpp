912a7876cf61417f86da7ad306224e7c7c514372
Harvey Nixon <Harveyn4444@gmail.com>
2024-06-04 23:18:03 +0100

    Update embedding-python.cpp

--- embedding-python/embedding-python.cpp
+++ embedding-python/embedding-python.cpp
@@ -2,8 +2,21 @@
 
 #define PY_SSIZE_T_CLEAN
 
-int main(){
+//#include<python.h>
+#include<I/Library/Frameworks/Python.framework/Versions/3.8/include/python3.8 -I/Library/Frameworks/Python.framework/Versions/3.8/include/python3.8>
+//#include<embedding-python/embedding-python.py>
 
-    
+
+int main(int argc, char const *argv[]){
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL)
+    if(program == NULL){
+        fprintf(stderr, "Error");
+        exit(1);
+    }
+
+    Py_SetProgramName(program);
+
+    Py_Initialize();
+
     return 0;
 }
