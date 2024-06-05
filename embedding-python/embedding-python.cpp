9292f29008b7c2ec80f4b59035e19cd9c6391c65
Harvey Nixon <Harveyn4444@gmail.com>
2024-06-05 11:23:21 +0100

    Update embedding-python.cpp

--- embedding-python/embedding-python.cpp
+++ embedding-python/embedding-python.cpp
@@ -1,22 +1,48 @@
-#include<iostream>
+// #include <iostream>
 
-#define PY_SSIZE_T_CLEAN
+// #define PY_SSIZE_T_CLEAN
 
-//#include<python.h>
-#include<I/Library/Frameworks/Python.framework/Versions/3.8/include/python3.8 -I/Library/Frameworks/Python.framework/Versions/3.8/include/python3.8>
+// #include </Library/Frameworks/Python.framework/Versions/3.8/include/python3.8/Python.h>
+
+//#include </usr/local/opt/python@3.8>
+//#include<-I/Library/Frameworks/Python.framework/Versions/3.8/include/python3.8/Python/Python.h>
 //#include<embedding-python/embedding-python.py>
 
 
-int main(int argc, char const *argv[]){
-    wchar_t *program = Py_DecodeLocale(argv[0], NULL)
-    if(program == NULL){
-        fprintf(stderr, "Error");
-        exit(1);
-    }
+// int main(int argc, char const *argv[]){
+//     wchar_t *program = Py_DecodeLocale(argv[0], NULL)
+//     if (program == NULL){
+//         fprintf(stderr, "Error");
+//         exit(1);
+//     }
 
-    Py_SetProgramName(program);
+//     Py_SetProgramName(program);
 
+//     Py_Initialize();
+
+//     if (Py_FinalizeEx() < 0){
+//         exit(120)
+//     }
+
+//     PyMem_RawFree(program);
+
+//     return 0;
+// }
+#include "initconfig.h"
+
+#include </Library/Frameworks/Python.framework/Versions/3.8/include/python3.8/Python.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    // Initialize the Python Interpreter
     Py_Initialize();
+
+    // Execute a simple Python script
+    PyRun_SimpleString("print('Hello from Python!')");
+
+    // Finalize the Python Interpreter
+    Py_Finalize();
 
     return 0;
 }
+
